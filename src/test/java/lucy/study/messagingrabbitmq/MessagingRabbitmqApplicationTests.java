00783aa2dce9b585881d40454840084a1dc83365
lucy <loveyou_06@naver.com>
2020-04-10 02:14:47 +0900

    init

---
+++ src/test/java/lucy/study/messagingrabbitmq/MessagingRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package lucy.study.messagingrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
